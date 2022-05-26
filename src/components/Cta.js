f0181502bb94043050bd51ce95ceed019133c5b4
andreyscott <andreyscott301@gmail.com>
2022-05-26 20:06:16 +0100

    made it with a bnag

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,42 +1,76 @@
 import React from 'react'
-// import man from 'logo512.png';
+//simport man from 'logo512.png';
 
 function Cta() {
   return (
   
-<div class="dark:bg-gray-800 bg-syellowLight flex relative z-20 items-center overflow-hidden">
-    <div class="container mx-auto  flex flex-col items-center md:flex-row md:justify-between relative py-14">
-        <div class="sm:w-2/4 lg:w-2/5 flex flex-col md:px-6 pb-4 relative z-20">
-        <div class="flex flex-col items-center">
-            <p class="justify-center text-center xl:text-3xl text-3xl h-24 xl:h-28 xl:py-3 w-96  font-black block  dark:text-white font-pop pb-2 text-sblue">
+//  <div class="dark:bg-gray-800 bg-white flex relative z-20 items-center overflow-hidden">
+//     <div class=" flex flex-col bg-black h-96 w-full  items-center md:flex-row md:justify-between  ">
+//         <div class="sm:w-2/4 lg:w-3/6  h- flex  flex-col md:px-6 pb-4 bg-green-300 ">
+//         <div class="flex flex-col bg-violet-500 h-fit w-full items-center">
+//             <p class="justify-center text-center font-bold text-tblue xl:text-5xl text-4xl bg-pink-400 h-24 xl:h-28   block  dark:text-white font-pop pb-2 text-sblue">
      
-            Send and Receive items from your doorstep to any location in the world
+//             Send and Receive items from your doorstep to any location in the world
            
-            </p>   
-            {/* dark:text-white */}
-            <p class="justify-center text-center xl:text-xl text-md h-24  px-2  font-black block  dark:text-white font-pop pb-2 text-gray-800">
+//             </p>   
+//             <p class="justify-center text-center xl:text-xl text-md h-24  px-2  font-black block  dark:text-white font-pop pb-2 text-gray-800">
           
-            Welcome to the Soft life! Use Topship to send items to your customers, friends, and family in Lagos, Abuja, London, Ghana, New York, Dubai and 200+ more cities. Say goodbye to stressful shipping forever!
-            </p>
-            </div>
+//             Welcome to the Soft life! Use T
+//             opship to send items to your customers, friends, and family in Lagos, Abuja, London, Ghana, New York, Dubai and 200+ more cities. Say goodbye to stressful shipping forever!
+//             </p>
+//             </div>
 
-            <div class="inline-flex justify-center items-center justify-items-center mt-8 overflow-hidden">
-                <a href="#" class="button bg-sblue border-2 border-transparent p-1 text-white text-md mr-4 hover:bg-syellow focus:outline-none focus:shadow-outline">
-                 learn More
-                </a>
-                <a href="#" class="button bg-transparent border-2 border-syellow text-sblue font-semibold dark:text-white hover:bg-sblue hover:text-white hover:border-transparent text-md">
-                    Sign in    
-                </a>
+//             <div class="inline-flex justify-center items-center justify-items-center mt-8 overflow-hidden">
+//                 <a href="#" class="button bg-sblue border-2 border-transparent p-1 text-white text-md mr-4 hover:bg-syellow focus:outline-none focus:shadow-outline">
+//                  learn More
+//                 </a>
+//                 <a href="#" class="button bg-transparent border-2 border-syellow text-sblue font-semibold dark:text-white hover:bg-sblue hover:text-white hover:border-transparent text-md">
+//                     Sign in    
+//                 </a>
+//             </div>
+//         </div>
+//         <div class="sm:block sm:w-2/4 lg:w-3/5 pt-2 justify-center h-fit items-center relative">
+//             <img 
+//                         src='https://www.designfreelogoonline.com/wp-content/uploads/2016/05/000718-Free-Logo-creator-Free-bag-shopping-logo-maker-03.png'
+//     alt='bag'
+//              class="max-w-xs md:max-w-sm m-auto"/>
+    
+//         </div>
+//     </div>
+// </div> 
+////////////////////////////
+ <div className="flex justify-center border-b border-black">
+<div className="max-w-7xl w-full bg-hero bg-hero-bottom
+    md:bg-hero-bottom-md sm:bg-hero-bottom-sm bg-no-repeat">
+    <div className="w-full pt-10 lg:pt-14 md:pt-12 xl:pt-20 pb-0 px-2  md:px-4 flex flex-col md:flex-row">
+        <div className="max-w-2xl md:w-1/2  ml-3    pb-16">
+            <div className="max-w-xl ">
+                <h2 className="text-4xl text-tblue font-medium lg:text-5xl md:text-left pb-4  lg:font-semibold md:block xl:text-6xl xl:font-bold ">
+                    Send and Receive items from your doorstep to any location in the world
+                </h2>
+               
+                <h3 className="text-xl lg:text-2xl  font-normal text-center md:text-left pt-4 mb-10 w-5/5">
+                    
+                    Welcome to the Soft life! Use Topship to send items to your customers, friends, and family in Lagos,
+                     Abuja, London, Ghana, New York, Dubai and 200+ more cities. Say goodbye to stressful shipping forever!
+                </h3>
+         <div className="flex sm:justify-center md:justify-start">
+         <a className="btnb bg-transparent text-black border-2  m-2 border-tblue pt-3 pb-3  h-12 font-bold text-lg md:text-base"
+                   href="https://medium.com/"> Learn More</a>
+                     <a className="btny m-2 h-12   py-2 font-bold text-lg md:text-base "
+                   href="https://medium.com/"> Get Started</a>
+                   </div>
             </div>
         </div>
-        <div class="sm:block sm:w-2/4 lg:w-3/5 pt-2 justify-center h-fit items-center relative">
-            <img 
-                        src='https://www.designfreelogoonline.com/wp-content/uploads/2016/05/000718-Free-Logo-creator-Free-bag-shopping-logo-maker-03.png'
-    alt='bag'
-             class="max-w-xs md:max-w-sm m-auto"/>
-        </div>
+        <div className="max-w-2xl  md:w-1/2  pb-10 mx-4">
+        <img
+                 src='https://www.designfreelogoonline.com/wp-content/uploads/2016/05/000718-Free-Logo-creator-Free-bag-shopping-logo-maker-03.png'
+/>
+</div>
     </div>
 </div>
+</div> 
+
 
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tyellow: "#ffd708",
+        tblue: "#2f4b8d",
+        tgreen: "#00a859",
+        tpurple: "#755cff",
+        syellow: "#f4b41a",
+        syellowLight: '#F9D78580',
+    },
+   
+    },
+     
+  
   },
+  
   plugins: [],
 }
